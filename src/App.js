cb8d1251cf366cfbc5a5b1c8d878f6cb13584784
mita4824 <87270333+mita4824@users.noreply.github.com>
2022-02-16 19:43:49 +0330

    learning sass

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
 import Burger from "./Burger";
+import "./css/style.css";
 function App() {
   return (
     <div className="App">
-       <Burger/>
-       <h1>jhgjghgjj</h1>
+      <Burger />
+      <h1>SASS</h1>
+      <h2>learning how to code</h2>
+      <p>
+        f766778as
+      </p>
     </div>
   );
 }
